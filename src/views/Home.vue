6d61ebdbe66d1c7bb929f53b4027e5efc0b9147f
Brolly0204 <102495553@qq.com>
2024-04-22 22:04:04 +0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import BarChartComp from '../components/BarChartComp.vue';
+// import BartStackComp from '../components/BartStackComp.vue';
+import LineChartComp from '../components/LineChartComp.vue';
+</script>
+
+<template>
+  <LineChartComp msg="普通折线图" />
+  <!-- <BartStackComp msg="堆叠瀑布图" />
+  <BarChartComp msg="普通bar" /> -->
+</template>
